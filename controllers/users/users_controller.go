a86c4859e2b669aa5e1d53843320941050e4679c
Kento Takano <kento2github@gmail.com>
2020-08-10 09:52:16 +0900

    user interface

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,29 @@
 package users
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/Kento75/bookstore_users-api/domain/users"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+	var user users.User
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		// TODO: handler error
+		return
+	}
+
+	// jsonをパース
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		// TODO: handler error
+		return
+	}
+
 	c.String(http.StatusNotImplemented, "Implement me !")
 }
 
