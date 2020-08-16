a4a94994e41e5ffd9c5417bbe9d6e09031c5edec
Kento Takano <kento2github@gmail.com>
2020-08-17 08:16:50 +0900

    user get routes 追加

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -11,11 +11,9 @@ import (
 
 const (
 	indexUniqueEmail = "email_UNIQUE"
+	errorNoRows      = "no rows in result set"
 	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
-)
-
-var (
-	usersDB = make(map[int64]*User)
+	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
 
 func something() {
@@ -28,21 +26,22 @@ func something() {
 }
 
 func (user *User) Get() *errors.RestErr {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.InternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
-	result := usersDB[user.Id]
+	result := stmt.QueryRow(user.Id)
 
-	if result == nil {
-		return errors.NotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		// 対象のデータが存在しない場合
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NotFoundError(fmt.Sprintf("user %d not found", user.Id))
+		}
+		fmt.Println(err)
+		return errors.InternalServerError(fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()))
 	}
-
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
 
 	return nil
 }
