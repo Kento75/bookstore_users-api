f5955e94a1f3ee7069def76e0d565f1875081ce9
Kento Takano <kento2github@gmail.com>
2020-08-18 08:28:43 +0900

    mysql utils 追加

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,18 +2,16 @@ package users
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/Kento75/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/Kento75/bookstore_users-api/utils/date_utils"
 	"github.com/Kento75/bookstore_users-api/utils/errors"
+	"github.com/Kento75/bookstore_users-api/utils/mysql_utils"
 )
 
 const (
-	indexUniqueEmail = "email_UNIQUE"
-	errorNoRows      = "no rows in result set"
-	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
-	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
+	queryGetUser    = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
 
 func something() {
@@ -34,13 +32,8 @@ func (user *User) Get() *errors.RestErr {
 
 	result := stmt.QueryRow(user.Id)
 
-	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
-		// 対象のデータが存在しない場合
-		if strings.Contains(err.Error(), errorNoRows) {
-			return errors.NotFoundError(fmt.Sprintf("user %d not found", user.Id))
-		}
-		fmt.Println(err)
-		return errors.InternalServerError(fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()))
+	if getErr := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); getErr != nil {
+		return mysql_utils.ParseError(getErr)
 	}
 
 	return nil
@@ -56,19 +49,15 @@ func (user *User) Save() *errors.RestErr {
 	user.DateCreated = date_utils.GetNowString()
 
 	// first_name, last_name, email, date_created
-	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
-	if err != nil {
-		// 一意制約違反の場合
-		if strings.Contains(err.Error(), indexUniqueEmail) {
-			return errors.BadRequestError(fmt.Sprintf("email %s already exists", user.Email))
-		}
-		return errors.InternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	insertResult, saveErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if saveErr != nil {
+		mysql_utils.ParseError(saveErr)
 	}
 
 	userId, err := insertResult.LastInsertId()
 
 	if err != nil {
-		return errors.InternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+		mysql_utils.ParseError(saveErr)
 	}
 
 	user.Id = userId
