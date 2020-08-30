586fd26e3c2038715766ac4414a707738e649cf8
Kento Takano <kento2github@gmail.com>
2020-08-30 19:57:47 +0900

    marshaller, crypto ロジック追加

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -31,7 +31,7 @@ func Get(c *gin.Context) {
 		c.JSON(getErr.Status, getErr)
 	}
 
-	c.JSON(http.StatusOK, user)
+	c.JSON(http.StatusOK, user.Marshall(c.GetHeader("X-Public") == "true"))
 }
 
 func Create(c *gin.Context) {
@@ -49,7 +49,7 @@ func Create(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusCreated, result)
+	c.JSON(http.StatusOK, result.Marshall(c.GetHeader("X-Public") == "true"))
 }
 
 func Update(c *gin.Context) {
@@ -74,7 +74,7 @@ func Update(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, result)
+	c.JSON(http.StatusOK, result.Marshall(c.GetHeader("X-Public") == "true"))
 }
 
 func Delete(c *gin.Context) {
@@ -100,5 +100,6 @@ func Search(c *gin.Context) {
 		c.JSON(err.Status, err)
 		return
 	}
-	c.JSON(http.StatusOK, users)
+
+	c.JSON(http.StatusOK, users.Marshall(c.GetHeader("X-Public") == "true"))
 }
--- services/users_service.go
+++ services/users_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"github.com/Kento75/bookstore_users-api/domain/users"
+	"github.com/Kento75/bookstore_users-api/utils/crypto_utils"
 	"github.com/Kento75/bookstore_users-api/utils/date_utils"
 	"github.com/Kento75/bookstore_users-api/utils/errors"
 )
@@ -25,6 +26,8 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	// "active"
 	user.Status = users.StatusActive
 	user.DateCreated = date_utils.GetNowDbFormat()
+	// パスワード暗号化
+	user.Password = crypto_utils.GetMd5(user.Password)
 	if err := user.Save(); err != nil {
 		return nil, err
 	}
@@ -67,7 +70,7 @@ func DeleteUser(userId int64) *errors.RestErr {
 	return user.Delete()
 }
 
-func Search(status string) ([]users.User, *errors.RestErr) {
+func Search(status string) (users.Users, *errors.RestErr) {
 	dao := &users.User{}
 	return dao.FindByStatus(status)
 }
